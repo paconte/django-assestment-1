d6d2eaba2ff8c5da9565867ece4be79726ffa03f
Francisco Revilla <paconte@gmail.com>
2022-10-25 15:24:30 +0200

    Run linters

--- abacum/abacum/wsgi.py
+++ abacum/abacum/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abacum.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abacum.settings")
 
 application = get_wsgi_application()
--- abacum/transactions/migrations/0001_initial.py
+++ abacum/transactions/migrations/0001_initial.py
@@ -7,17 +7,24 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Transaction',
+            name="Transaction",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('date', models.DateField()),
-                ('account', models.PositiveBigIntegerField(db_index=True)),
-                ('amount', models.DecimalField(decimal_places=2, max_digits=9)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("date", models.DateField()),
+                ("account", models.PositiveBigIntegerField(db_index=True)),
+                ("amount", models.DecimalField(decimal_places=2, max_digits=9)),
             ],
         ),
     ]
